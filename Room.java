38e5a7019fe486e15f883123b08f1de536480a6d
ansonhei <anson0607cch@gmail.com>
2025-01-24 13:28:12 +1100

    Added default requested T of the building and methods for temperature control

--- Room.java
+++ Room.java
@@ -5,6 +5,7 @@ public class Room {
     private double currentT;
     private boolean heatingEnabled;
     private boolean coolingEnabled;
+    public double defaultRequestedTemperature = 20.0;
 
 
     public Room(int id) {
@@ -56,11 +57,50 @@ public class Room {
         return min + (max - min) * random.nextDouble();
     }
 
+    /*
+     * Simulate temperature changes based on the heating and cooling status of the room.    
+     * 
+     * @param check if heating is enabled, false otherwise
+     * @param check if cooling is enabled, false otherwise
+     * if heating is enabled, increase the current temperature by 0.5
+     * if cooling is enabled, decrease the current temperature by 0.5
+     */
+    public void updateTemperature(boolean heating, boolean cooling) {
+        if (heating) {
+            currentT += 0.5; // Simulate heating effect
+        } else if (cooling) {
+            currentT -= 0.5; // Simulate cooling effect
+        }
+    }
+
+    /*
+     * Set the temperature control for the room based on the requested temperature and tolerance.
+     * 
+     * @param requestedTemperature the desired temperature for the room
+     * @param tolerance the tolerance range for temperature control
+     * if the current temperature is less than the requested temperature minus the tolerance, enable heating
+     * if the current temperature is greater than the requested temperature plus the tolerance, enable cooling
+     * otherwise, disable both heating and cooling
+     */
+    public void setTemperatureControl(double requestedTemperature, double tolerance) {
+        if (currentT < requestedTemperature - tolerance) {
+            heatingEnabled = true;
+            coolingEnabled = false;
+        } else if (currentT > requestedTemperature + tolerance) {
+            coolingEnabled = true;
+            heatingEnabled = false;
+        } else {
+            heatingEnabled = false;
+            coolingEnabled = false;
+        }
+    }
+    
+
     // toString method
     @Override
     public String toString() {
-        return "Room [id=" + id + ", currentT=" + currentT + ", heatingEnabled=" + heatingEnabled + ", coolingEnabled="
-                + coolingEnabled + "]";
+        return "Room: id=" + id + ", currentT=" + currentT + ", heatingEnabled=" + heatingEnabled + ", coolingEnabled="
+                + coolingEnabled;
     }
 
 }
